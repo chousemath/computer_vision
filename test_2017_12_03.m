cecbe9b695c8d055b783f6b3651b8e7e20601a8d
Joseph Sungpil Choi <chousemath@gmail.com>
2017-12-04 10:04:25 +0900

    Add photos (matrix of vectors of intensity)

--- test_2017_12_03.m
+++ test_2017_12_03.m
@@ -6,6 +6,7 @@ pkg load image;
 img = imread('img/tesla_model_s.jpg');
 img_gray = rgb2gray(img);
 imshow(img_gray);
+waitforbuttonpress;
 % get the size of this image
 disp(size(img_gray));
 % get the value at row 50, column 100
@@ -22,7 +23,8 @@ disp(img_gray(101:103, 201:203));
 img2 = imread('img/forest.jpg');
 % get only the red channel of the image
 img2red = img2(:, :, 1);
-figure, imshow(img2red);
+imshow(img2red);
+waitforbuttonpress;
 % When doing integer division Octave will round the result to the nearest integer. 
 % This is different from most programming languages, where the result is often 
 % floored to the nearest integer. So, the result of int32 (5) ./ int32 (8) is 1.
@@ -34,13 +36,28 @@ img_columns_half = img_columns / 2;
 line([1, img_columns], [img_rows_half, img_rows_half], 'color', 'r');
 line([img_columns_half, img_columns_half], [1, img_rows], 'color', 'b');
 % plot the first row of an image
-figure, plot(img2red(1,:));
-
+plot(img2red(1,:));
+waitforbuttonpress;
 % load an image and crop it
 img_bicycle = imread('img/bicycle.jpg');
-figure, imshow(img_bicycle);
+imshow(img_bicycle);
+waitforbuttonpress;
 img_bicycle_size = size(img_bicycle);
 img_bicycle_rows = img_bicycle_size(1);
 img_bicycle_columns = img_bicycle_size(2);
 % image is converted to b/w automatically
-figure, imshow(img_bicycle(:, 1:img_bicycle_columns/2));
+imshow(img_bicycle(:, 1:img_bicycle_columns/2));
+waitforbuttonpress;
+
+% define a function for scaling
+function result = scale(img, value)
+    result = value .* img;
+endfunction
+
+% load two images
+ocean1 = imread('img/ocean_1.jpg');
+ocean2 = imread('img/ocean_2.jpg');
+
+ocean3 = scale(ocean1, 0.5) + scale(ocean2, 0.5);
+imshow(ocean3);
+waitforbuttonpress;
